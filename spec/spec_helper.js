0f18b566f2c17bac2459c0e3d98ed8af051fb290
Florian Hanke <florian.hanke@gmail.com>
2009-07-06 14:01:20 +0200

    + pending, + describe aliased to context, + it_should_translate

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -5,7 +5,8 @@
 //
 // Output is intended to be used in a html wrapper, as in TextMate.
 
-// The tester function;
+// The tester function.
+//
 it = function(should, callback) {
   var result = callback();
   var text = this.context_description + ' ' + should + '.<br/>';
@@ -16,18 +17,61 @@ it = function(should, callback) {
   };
 };
 
-// For defining a context
+// For defining a context.
+//
 context = function(description, setup, specs, breakdown) {
   var old_description = this.context_description;
   var pre_description = old_description ? old_description + ' ' : '';
-  
+
   // framework setup
   this.context_description = pre_description + description; // sets the description on the global object
   
   if (setup)     { setup(); };
   specs();
   if (breakdown) { breakdown(); };
-  
+
   // framework breakdown
   this.context_description = old_description;
+};
+
+// Alias describe to context.
+//
+describe = context;
+
+// Pending.
+//
+pending = function(description) {
+  print('<div style="background-color:yellow;">Pending: ' + this.context_description + ' ' + description + '</div>');
+};
+
+// Use as follows:
+//
+// describe('translations',
+//  null,
+//  function specs() {
+//    var l;
+//    context('de',
+//      function setup() { l = 'de'; },
+//      function specs() {
+//        it_should_translate(l, 'gaga', 'Translation missing: gaga');
+//        it_should_translate(l, 'common.name', 'Name');
+//      }
+//    );
+//    context('fr',
+//      function setup() { l = 'fr'; },
+//      function specs() {
+//        it_should_translate(l, 'gaga', 'Translation missing: gaga');
+//        it_should_translate(l, 'common.name', 'nom');
+//      }
+//    );
+//
+it_should_translate = function(language, key, expected) {
+  it('should translate the key with ' + key + ' into ' + expected + ' (' + language + ')', function() {
+    var translated = t(key, language);
+    var result = (translated == expected);
+    if (!result) {
+      print('Expected ' + expected + ', got ' + translated);
+    };
+    return result;
+  });
 };
